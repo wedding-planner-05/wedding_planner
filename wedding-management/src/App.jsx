63db1cba53852a9da649fa1e25062a19abd819b6
Devendra <devendravishvakarma128@gmail.com>
2024-04-05 22:58:24 +0530

    checkFooter

--- wedding-management/src/App.jsx
+++ wedding-management/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import "./Components/Footer.css"
+import Footer from "./Components/Footer";
 import Home from "./Home-Page/Home";
 
 function App() {
   return (
     <>
       <Home />
+      <Footer/>
     </>
   );
 }
