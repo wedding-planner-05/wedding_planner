a7313b6fd269bcabcf76e9668701a5c52bf95ce8
wedding-project <wedding.planner.techwizards@gmail.com>
2024-04-06 14:52:51 +0530

    vendor-category-added-2

--- wedding-management/src/Components/Vendors.jsx
+++ wedding-management/src/Components/Vendors.jsx
@@ -1,34 +1,94 @@
- import React from 'react'
- import './Vendor.css'
-import { FaHeartPulse } from 'react-icons/fa6'
+import React from "react";
+import "./Vendor.css";
+import { FaHeartPulse } from "react-icons/fa6";
 
 const Vendors = () => {
   return (
-    <div className='vendor-div container mt-5 p-5'>
-        <h2 style={{fontSize:"1.5vw"}} className='text-center'>Find trusted wedding services in all Indore area’s & Book Verified Vendors in simple steps.</h2>
-      
-      <div className='vendor-image-div mt-5' >
+    <div className="vendor-div container mt-5 p-5">
+      <h2 style={{ fontSize: "1.5vw" }} className="text-center">
+        Find trusted wedding services in all Indore area’s & Book Verified
+        Vendors in simple steps.
+      </h2>
 
-        <div className='vendor-image'>
-        <a href=""><img src="/images/vendor1.png" alt="" /></a>
-        <a href=""><img src="/images/vendor2.png" alt="" /></a>
-        </div>            
-        
-        <div className='vendor-image mt-3'>
-        <div className='text-center'>Vendors Category <br /> <span style={{color:"crimson"}}>_________________ <FaHeartPulse/>  _________________</span><br /></div>
+      <div className="vendor-category mt-5" >
+        <div
+          className="vendor-img d-flex justify-content-center  align-item-center"
+          style={{
+            width: "300px",
+            height: "400px",
+          }}
+        >
+          <img src="/images/vendor1.png" alt="teri " />
+          <div className="vendor-button">
+            <button className="btn btn-warning text-white">View More</button>
+          </div>
+        </div>
 
-        <a href=""><img src="/images/vendor3.png" alt="" /></a>
-        <a href=""><img src="/images/vendor4.png" alt="" /></a>
-        </div>            
-        
-        <div className='vendor-image'>
-        <a href=""><img src="/images/vendor5.png" alt="" /></a>
-        <a href=""><img src="/images/vendor6.png" alt="" /></a>
-        </div>            
-      
+        <div
+          className="vendor-img d-flex justify-content-center align-item-center"
+          style={{
+            width: "300px",
+            height: "400px",
+          }}
+        >
+          <img src="/images/vendor2.png" alt="teri " />
+          <div className="vendor-button">
+            <button className="btn btn-danger text-white">View More</button>
+          </div>
+        </div>
+
+        <div
+          className="vendor-img d-flex justify-content-center align-item-center"
+          style={{
+            width: "300px",
+            height: "400px",
+          }}
+        >
+          <img src="/images/vendor3.png" alt="teri " />
+          <div className="vendor-button">
+            <button className="btn btn-warning text-white">View More</button>
+          </div>
+        </div>
+
+        <div
+          className="vendor-img d-flex justify-content-center align-item-center"
+          style={{
+            width: "300px",
+            height: "400px",
+          }}
+        >
+          <img src="/images/vendor4.png" alt="teri " />
+          <div className="vendor-button">
+            <button className="btn btn-warning text-white">View More</button>
+          </div>
+        </div>
+        <div
+          className="vendor-img d-flex justify-content-center align-item-center"
+          style={{
+            width: "300px",
+            height: "400px",
+          }}
+        >
+          <img src="/images/vendor5.png" alt="teri " />
+          <div className="vendor-button">
+            <button className="btn btn-warning text-white">View More</button>
+          </div>
+        </div>
+        <div
+          className="vendor-img d-flex justify-content-center align-item-center"
+          style={{
+            width: "300px",
+            height: "400px",
+          }}
+        >
+          <img src="/images/vendor6.png" alt="teri " />
+          <div className="vendor-button">
+            <button className="btn btn-warning text-white">View More</button>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Vendors
+export default Vendors;
