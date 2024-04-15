0f9594e1d8f5292eca67608aa175cdc5c7d6749b
priyanshi soni <priyanshisoni7227@gmail.com>
2024-04-15 09:27:18 +0530

    backend

---
+++ wedding-management/src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="container-fluid mt-5">
+      <div className="text-center">
+        <h4 className="" style={{ color: "crimson" }}>
+          About us
+        </h4>
+      </div>
+      <div
+        className="row d-flex justify-content-around  py-2 m-1"
+        style={{
+          borderTop: "0px solid ",
+          borderBottom: "35px solid crimson",
+          borderLeft: "20px solid crimson",
+          borderRight: "20px solid crimson",
+        }}
+      >
+        <div className="col-md-6 p-5">
+          <p>
+            Welcome to the National Hookah Community Association "NHCA". We are
+            a diverse alliance of businesses from all ends of the Hookah
+            experience, from manufacturers and importers of molasses, pipes and
+            accessories to distributors, Hookah lounges and Hookah/shisha retail
+            stores. If you are a Hookah business, please join us and help us
+            defend and protect your business, our culture and community. If you
+            are an interested member of the public, welcome. Please enjoy
+            learning about Hookah and its unique culture and practice.
+          </p>
+        </div>
+        <div className="px-5 col-md-6 p-4 d-flex">
+          <img
+            className="img-fluid custom-imgs "
+            src="images/p6.webp"
+            alt="Image not display"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
--- wedding-management/src/Home-Page/Home.jsx
+++ wedding-management/src/Home-Page/Home.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Header from "../Components/Header/Header";
 
-// import AboutUs from "../Components/About/AboutUs";
+
 
 import Vendors from "../Components/Vendor/Vendors";
 import ContactUs from "../Components/Contact/ContactUs";
+import Footer from "../Components/Footer/Footer";
+import AboutUs from "../Components/AboutUs/AboutUs";
 
 function Home() {
   return (
@@ -14,8 +16,9 @@ function Home() {
       <Navbar />  
       <Header />
       <Vendors />
-      {/* <AboutUs /> */}
+      <AboutUs/>
       <ContactUs />
+      <Footer/>
     </>
   );
 }
