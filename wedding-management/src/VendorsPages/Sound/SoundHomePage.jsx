653372a4aa5ae90706a95cbc1618a670747c0760
abhimanyu <wedding.planner.techwizards@gmail.com>
2024-04-22 23:54:01 +0530

    Revert "all vendors connect with api-gate-way"
    This reverts commit 1362cb9703943ad1656d82baa7f5e5af4a3da9cf.

---
+++ wedding-management/src/VendorsPages/Sound/SoundHomePage.jsx
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import React, {useEffect, useState } from 'react'
+import { FaMapMarkerAlt, FaRupeeSign, FaStar } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom';
+
+
+const SoundHomePage = () => {
+    const [products,setProducts] = useState([]) ;
+    const navigate = useNavigate()
+
+
+    useEffect(()=>{
+        axios.get("http://localhost:3000/sound/soundInfo/viewAllVendors").then((response)=>{
+        setProducts(response.data.data)
+        }).catch()
+    },[])
+
+    const SoundVendorDetails = (data)=>{
+        navigate("/SoundVendorDetails",{state:data})
+    }
+
+  return <>
+  
+    <SoundData.Provider value={products}>
+
+    <div className="container-fluid d-flex flex-wrap justify-content-evenly align-items-center">
+      {products.map((product, index) => (
+        <section onClick={()=>SoundVendorDetails(product)} key={index} className="main-page m-3">
+          <div
+            key={index}
+            className="p-2 row details-block "
+          >
+            <div className="p-0">
+              <img
+                className="img-fluid custom-img"
+                src={product.imageUrl}
+                alt={product.name}
+              />
+            </div>
+            <div className="p-1 font-size">
+              <div className="row">
+                <div className="col">
+                  <div className="h6" style={{ width: "170%" }}>
+                    <strong>{product.name}</strong>
+                  </div>
+                  <p className="custom-text-size">Photo + Video</p>
+                </div>
+                <div className="col text-end">
+                  <p className="h6">
+                    <FaStar color="crimson" /> {product.rating || "N/A"}
+                  </p>
+                  <p className="font custom-text-size">
+                    <FaMapMarkerAlt color="green" /> {product.address.slice(0,14) + ".."}
+                  </p>
+                </div>
+              </div>
+              <h6 className="mb-0">
+                <FaRupeeSign /> {product.serviceCharge || "Price not available"}{" "}
+                Onwards
+              </h6>
+            </div>
+          </div>
+        </section>
+      ))}
+    </div>
+
+    </SoundData.Provider>
+  
+  </>
+}
+
+export default SoundHomePage
