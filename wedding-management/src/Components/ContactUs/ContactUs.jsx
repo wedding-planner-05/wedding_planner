33c8e26c2f9c421816d10614eb73c0365657cba1
Satish <satishsjr101@gmail.com>
2024-05-24 09:44:40 +0530

    Contact Us Page UI updated...

--- wedding-management/src/Components/ContactUs/ContactUs.jsx
+++ wedding-management/src/Components/ContactUs/ContactUs.jsx
@@ -13,7 +13,7 @@ const ContactUs = () => {
     <div className="container-fluid mt-5 p-0" id="Contactus">
       {/* Contact Information Section */}
       <div className="text-center m-4 ">
-        <h4 style={{ color: "crimson" }}>Contact us</h4>
+        <h2 style={{ color: "crimson" }}>Contact us</h2>
         <span className="custom-icons" style={{ color: "crimson" }}>
           ___________
           <FaHeartPulse />
@@ -25,24 +25,25 @@ const ContactUs = () => {
       </div>
 
       {/* Main Content Section */}
-      <section className="d-flex justify-content-center align-content-center mb-4">
+      <section className="container mb-5">
         <div className="row contact-main p-3">
+
           {/* Contact Information Left Section */}
-          <div className="col-lg-12 col-md-12 p-3 d-flex flex-column justify-content-evenly align-items-center border-1" style={{backgroundColor:'crimson',color:'white'}}>
-            <h4>Contact Information</h4>
-            <div className="d-flex flex-column justify-content-evenly text-size-s row-gap-3">
-              <span className="">
-                <FaPhoneAlt /> +91 7354560590
+          <div className="col-lg-12 col-md-12 p-3 d-flex flex-column justify-content-evenly align-items-center border-1 rounded-3" style={{backgroundColor:'crimson',color:'white'}}>
+            <h4 className="mb-4">Contact Information</h4>
+            <div className="d-flex flex-column justify-content-evenly text-size-s row-gap-4">
+              <span className="" style={{fontSize:'19px'}}>
+                <FaPhoneAlt size={'23px'}/>  +91 7354560597
               </span>
-              <span className="">
-                <FaMapMarkerAlt /> wedding.planner.techwizards@gmail.com
+              <span className="" style={{fontSize:'19px'}}>
+              <FaEnvelope size={'23px'}/>  wedding.planner.techwizards@gmail.com
               </span>
-              <span className="">
-                <FaEnvelope /> C21 Mall, Vijay Nagar Square, Indore
+              <span className="" style={{fontSize:'19px'}}>
+              <FaMapMarkerAlt size={'23px'}/> InfoTech Designer Company, C21 Mall, Vijay Nagar Square, Indore
               </span>
 
               <div className="socialiconbox">
-                <ul className="socialicon d-flex justify-content-between px-2">
+                <ul style={{width:'90%'}} className="socialicon d-flex justify-content-between px-2 h6">
                   <li>
                     <i className="bx bxl-instagram"></i>
                   </li>
@@ -61,12 +62,11 @@ const ContactUs = () => {
           </div>
 
           {/* Contact Form Right Section */}
-          <div className="col-lg-12 col-md-6"> 
-            {/* Form Fields */}
+          {/* <div className="col-lg-12 col-md-6"> 
             <div className="row m-3">
               <div className="col-sm-6">
                 <div className="form-group text-size-s">
-                  <label htmlFor="firstName" style={{width: '100%'}}>First Name</label> {/* Adjusted label width */}
+                  <label htmlFor="firstName" style={{width: '100%'}}>First Name</label> 
                   <input
                     className="form-control rounded-0 custom-input-w"
                     type="text"
@@ -76,7 +76,7 @@ const ContactUs = () => {
               </div>
               <div className="col-sm-6">
                 <div className="form-group text-size-s">
-                  <label htmlFor="lastName" style={{width: '100%'}}>Last Name</label> {/* Adjusted label width */}
+                  <label htmlFor="lastName" style={{width: '100%'}}>Last Name</label> 
                   <input
                     className="form-control rounded-0 custom-input-w custom-input-w"
                     type="text"
@@ -86,11 +86,10 @@ const ContactUs = () => {
               </div>
             </div>
 
-            {/* Email and Phone Number Fields */}
             <div className="row m-3">
               <div className="col-sm-6">
                 <div className="form-group text-size-s">
-                  <label htmlFor="email" style={{width: '100%'}}>Email</label> {/* Adjusted label width */}
+                  <label htmlFor="email" style={{width: '100%'}}>Email</label> 
                   <input
                     className="form-control rounded-0 custom-input-w"
                     type="email"
@@ -100,7 +99,7 @@ const ContactUs = () => {
               </div>
               <div className="col-sm-6">
                 <div className="form-group text-size-s">
-                  <label htmlFor="phone" style={{width: '100%'}}>Phone Number</label> {/* Adjusted label width */}
+                  <label htmlFor="phone" style={{width: '100%'}}>Phone Number</label> 
                   <input
                     className="form-control rounded-0 custom-input-w"
                     type="tel"
@@ -110,7 +109,6 @@ const ContactUs = () => {
               </div>
             </div>
 
-            {/* Select Subjects Section */}
             <div className="m-4">
               <h6 className="text-dark">Select Subjects</h6>
               <div className="form-check form-check-inline">
@@ -143,7 +141,6 @@ const ContactUs = () => {
               </div>
             </div>
 
-            {/* Message Section */}
             <div>
               <h6 className="text-size-s m-4">Message</h6>
               <div className="form-check form-check-inline">
@@ -156,13 +153,13 @@ const ContactUs = () => {
               </div>
             </div>
 
-            {/* Send Button */}
             <div className="text-end p-4 pe-0">
               <button className="btn text-white" style={{ backgroundColor: "crimson" }}>
                 Send Message
               </button>
             </div>
-          </div>
+          </div> */}
+          
         </div>
       </section>
     </div>
--- wedding-management/src/VendorsPages/Garden/GardenHomePage.jsx
+++ wedding-management/src/VendorsPages/Garden/GardenHomePage.jsx
@@ -1,43 +1,46 @@
-import { TextField } from '@mui/material';
-import axios from 'axios';
-import { useEffect, useState } from 'react'
-import { FaMapMarkerAlt, FaRupeeSign, FaStar } from 'react-icons/fa';
-import { useLocation, useNavigate } from 'react-router-dom';
-import './GardenDetail.css'
+import { TextField } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FaMapMarkerAlt, FaRupeeSign, FaStar } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./GardenDetail.css";
 // import { useNavigate } from 'react-router-dom';
 
 const GardenHomePage = () => {
-  const [products,setProducts] = useState([]) ;
-  
-  const [minValue,setMinValue] = useState(0) ;
-  const [maxValue,setMaxValue] = useState(1000000) ;
+  const [products, setProducts] = useState([]);
+
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(1000000);
   const [isProductAvailable, setProductAvailable] = useState(true);
   const [inputText, setInputText] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-      axios.get("http://localhost:3000/garden/garden/viewAllVendors").then((response)=>{
-      console.log(response.data.data)
-      setProducts(response.data.data)
-      }).catch(err=>{
-        console.log(err);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/garden/garden/viewAllVendors")
+      .then((response) => {
+        console.log(response.data.data);
+        setProducts(response.data.data);
       })
-  },[])
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-const GardenVendorDetails = (data)=>{
-  navigate("/GardenVendorDetails",{state:data})
-}
-const handlerViewall = (min, max) => {
-  setMinValue(min);
-  setMaxValue(max);
-  setProductAvailable(true); // Reset product availability flag
-}
+  const GardenVendorDetails = (data) => {
+    navigate("/GardenVendorDetails", { state: data });
+  };
+  const handlerViewall = (min, max) => {
+    setMinValue(min);
+    setMaxValue(max);
+    setProductAvailable(true); // Reset product availability flag
+  };
 
-  const filterHandeler = (ele)=>{
-      // return priceFilter.price? priceFilter.operation=='<=' ? ele.serviceCharge <= priceFilter.price : ele.serviceCharge >= priceFilter.price  : true
-      return ele.price >= minValue && ele.price <= maxValue 
-  }
+  const filterHandeler = (ele) => {
+    // return priceFilter.price? priceFilter.operation=='<=' ? ele.serviceCharge <= priceFilter.price : ele.serviceCharge >= priceFilter.price  : true
+    return ele.price >= minValue && ele.price <= maxValue;
+  };
 
   let inputHandler = (e) => {
     var lowerCase = e.target.value.toLowerCase();
@@ -45,89 +48,177 @@ const handlerViewall = (min, max) => {
     setInputText(lowerCase);
   };
 
-
-return  <> 
-<div className='vendors-box mt-5  d-flex justify-content-between'>
-  <div className='filter-box'>
-  <div className='filter-box-inner d-flex flex-column align-items-center justify-content-center gap-4 '>
-    {/* <button onClick={()=>setPriceFilter({operation :"",price:0})} className='btn' style={{height:'40px',width:"110px" , border:'1px solid crimson'}}>view all</button> */}
-    <button onClick={()=>handlerViewall(0,1000000)} className='btn' style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}}>view all</button>
-    <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(0,300000)} className='btn'>Below 300000</button>
-    <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(300000,500000)} className='btn'>30000-500000</button>
-    <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(500000,700000)} className='btn'>500000-700000</button>
-    {/* <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(15000,20000)} className='btn'>15000-20000</button> */}
-    {/* <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(20000,25000)} className='btn'>20000-25000</button> */}
-    {/* <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(25000,30000)} className='btn'>25000-30000</button> */}
-    <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(700000,2000000)} className='btn'>Above 700000</button>
-  </div> 
-  </div>
-
-  <div className ='cards'>
-    <div style={{width:"70%"}} className="main">
-        {/* <p>Search Vendors</p> */}
-        <div className="search mt-5">
-          <TextField
-            id="outlined-basic"
-            onChange={inputHandler}
-            variant="standard"
-            fullWidth
-            label="search"
-          />
-        </div>
-      {/* <List input={inputText} /> */}
-    </div>
-  {products.filter(filterHandeler).length === 0 && isProductAvailable ? 
-        <h3>No products available in the selected price range</h3> : 
-        <div className="d-flex  flex-wrap justify-content-evenly align-items-center">
-        {products.filter((ele)=>filterHandeler(ele) && ele.title.toLowerCase().includes(inputText.toLowerCase()) ).map((product, index) => (
-          <section onClick={()=>GardenVendorDetails(product)} key={index} className="main-page m-3">
-            <div style={{cursor:'pointer'}}
-              key={index}
-              className="p-2 row details-block "
+  return (
+    <>
+      <div className="vendors-box mt-5  d-flex justify-content-between">
+        <div className="filter-box">
+          <div className="filter-box-inner d-flex flex-column align-items-center justify-content-center gap-4 ">
+            {/* <button onClick={()=>setPriceFilter({operation :"",price:0})} className='btn' style={{height:'40px',width:"110px" , border:'1px solid crimson'}}>view all</button> */}
+            <button
+              onClick={() => handlerViewall(0, 1000000)}
+              className="btn"
+              style={{
+                height: "40px",
+                width: "150px",
+                color: "black",
+                borderRadius: "20px",
+                backgroundColor: "white",
+                border: "3px solid crimson",
+              }}
             >
-              <div className="p-0">
-                <img style={{width: "100%",height: "200px"}}
-                  className=" custom-img"
-                  src={ product.imageUrl}
-                  // src={`http://localhost:3003/`+ product.imageUrl}
-                  alt={product.title}
-                />
-              </div>
-              <div className="p-1 font-size">
-                <div className="row">
-                  <div className="col">
-                    <div className="h6" style={{ width: "170%" }}>
-                      <strong>{product.title}</strong>
-                    </div>
-                    {/* <p className="custom-text-size">Photo + Video</p> */}
-                  </div>
-                  <div className="col text-end">
-                    <p className="h6">
-                      <FaStar color="crimson" /> {product.rating || "N/A"}
-                    </p>
-                    <p className="font custom-text-size">
-                      <FaMapMarkerAlt color="green" /> {product.location}
-                    </p>
-                  </div>
-                </div>
-                <h6 className="mb-0">
-                  <FaRupeeSign /> {product.price || "Price not available"}{" "}
-                  Onwards
-                </h6>
-              </div>
+              view all
+            </button>
+            <button
+              style={{
+                height: "40px",
+                width: "150px",
+                color: "black",
+                borderRadius: "20px",
+                backgroundColor: "white",
+                border: "3px solid crimson",
+              }}
+              onClick={() => handlerViewall(0, 300000)}
+              className="btn"
+            >
+              Below 300000
+            </button>
+            <button
+              style={{
+                height: "40px",
+                width: "150px",
+                color: "black",
+                borderRadius: "20px",
+                backgroundColor: "white",
+                border: "3px solid crimson",
+              }}
+              onClick={() => handlerViewall(300000, 500000)}
+              className="btn"
+            >
+              30000-500000
+            </button>
+            <button
+              style={{
+                height: "40px",
+                width: "150px",
+                color: "black",
+                borderRadius: "20px",
+                backgroundColor: "white",
+                border: "3px solid crimson",
+              }}
+              onClick={() => handlerViewall(500000, 700000)}
+              className="btn"
+            >
+              500000-700000
+            </button>
+            {/* <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(15000,20000)} className='btn'>15000-20000</button> */}
+            {/* <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(20000,25000)} className='btn'>20000-25000</button> */}
+            {/* <button style={{height:'40px',width:"150px" ,color:'black',borderRadius:'20px',backgroundColor:'white', border:'3px solid crimson'}} onClick={()=>handlerViewall(25000,30000)} className='btn'>25000-30000</button> */}
+            <button
+              style={{
+                height: "40px",
+                width: "150px",
+                color: "black",
+                borderRadius: "20px",
+                backgroundColor: "white",
+                border: "3px solid crimson",
+              }}
+              onClick={() => handlerViewall(700000, 2000000)}
+              className="btn"
+            >
+              Above 700000
+            </button>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div style={{ width: "70%" }} className="main">
+            {/* <p>Search Vendors</p> */}
+            <div className="search mt-5">
+              <TextField
+                id="outlined-basic"
+                onChange={inputHandler}
+                variant="standard"
+                fullWidth
+                label="search"
+              />
             </div>
-          </section>
-        ))
-        }
-  </div>
-}      
-    </div>
-  </div>
-</>
-}
-export default GardenHomePage
- //.slice(0,13) + ".."
-{/* <>
+            {/* <List input={inputText} /> */}
+          </div>
+          {products.filter(filterHandeler).length === 0 &&
+          isProductAvailable ? (
+            <h3>No products available in the selected price range</h3>
+          ) : (
+            <div className="d-flex  flex-wrap justify-content-evenly align-items-center">
+              {products
+                .filter(
+                  (ele) =>
+                    filterHandeler(ele) &&
+                    ele.title.toLowerCase().includes(inputText.toLowerCase())
+                )
+                .map((product, index) => (
+                  <section
+                    onClick={() => GardenVendorDetails(product)}
+                    key={index}
+                    className="main-page m-3"
+                  >
+                    <div
+                      style={{ cursor: "pointer" }}
+                      key={index}
+                      className="p-2 row details-block "
+                    >
+                      <div className="p-0">
+                        <img
+                          style={{ width: "100%", height: "200px" }}
+                          className=" custom-img"
+                          src={
+                            product.imageUrl
+                              ? product.imageUrl
+                              : `http://localhost:3003/` +
+                                product.imageUrl +
+                                ".png"
+                          }
+                          // src={`http://localhost:3003/`+ product.imageUrl}
+                          alt={product.title}
+                        />
+                      </div>
+                      <div className="p-1 font-size">
+                        <div className="row">
+                          <div className="col">
+                            <div className="h6" style={{ width: "170%" }}>
+                              <strong>{product.title}</strong>
+                            </div>
+                            {/* <p className="custom-text-size">Photo + Video</p> */}
+                          </div>
+                          <div className="col text-end">
+                            <p className="h6">
+                              <FaStar color="crimson" />{" "}
+                              {product.rating || "N/A"}
+                            </p>
+                            <p className="font custom-text-size">
+                              <FaMapMarkerAlt color="green" />{" "}
+                              {product.location}
+                            </p>
+                          </div>
+                        </div>
+                        <h6 className="mb-0">
+                          <FaRupeeSign />{" "}
+                          {product.price || "Price not available"} Onwards
+                        </h6>
+                      </div>
+                    </div>
+                  </section>
+                ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+export default GardenHomePage;
+//.slice(0,13) + ".."
+{
+  /* <>
     <div className="container-fluid pt-5 d-flex flex-wrap justify-content-evenly align-items-center">
       {products.map((product, index) => (
         <section key={index} className="main-page m-3">
@@ -172,4 +263,5 @@ export default GardenHomePage
     </div>
 
 
-</> */}
+</> */
+}
